ce816fa81297b3c9dbda3a0477c27ccad55e45dd
Cod <cod3eater@gmail.com>
2026-07-13 18:59:28 +0200

    packaging: update metadata for v1.1.1

--- Formula/pleiades.rb
+++ Formula/pleiades.rb
@@ -3,25 +3,25 @@ class Pleiades < Formula
   homepage "https://github.com/CodWasTaken/Pleiades"
   head "https://github.com/CodWasTaken/Pleiades.git", branch: "master"
   license "MIT"
-  version "1.1.0"
+  version "1.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.1.0/pleiades-macos-arm64.tar.gz"
-      sha256 "d6fcdba9fe01dca9c52ed7db5ccb0e391578b21b8d3909fb29d900d04f267a3b"
+      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.1.1/pleiades-macos-arm64.tar.gz"
+      sha256 "b6ce3c68bb67beba5704c274c17fbd142d8dbd0533103b7d6686220a8c5f590f"
     else
-      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.1.0/pleiades-macos-amd64.tar.gz"
-      sha256 "514517f41dabc0dcf74c0cb02da4f2b20a958a5cc857ee6bdb5a7fc76c306c62"
+      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.1.1/pleiades-macos-amd64.tar.gz"
+      sha256 "91690bd6ba20117051a015a6e3ec04dc2d00ce65368681e433af4b7030c67f35"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.1.0/pleiades-linux-arm64.tar.gz"
-      sha256 "b90c5213e62cad5d4c8a76be2b27230059403521687e6a3439bc1098429e64e5"
+      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.1.1/pleiades-linux-arm64.tar.gz"
+      sha256 "1ee4371803eef2b9cd74cd0ffd7af45c09921b91b10ee36677426120d6957055"
     else
-      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.1.0/pleiades-linux-amd64.tar.gz"
-      sha256 "3231e131fbb8678534045daea9c054e2add5e41380e622e26d291b7dc786836e"
+      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.1.1/pleiades-linux-amd64.tar.gz"
+      sha256 "a316ffd47d3986ea5aca577b1b7b9f5e10c711de1af614ba487558abff6263c5"
     end
   end
 
