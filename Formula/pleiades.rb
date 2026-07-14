33cac8b01e9640d61f5cc910d4341389c266cb4f
Cod <cod3eater@gmail.com>
2026-07-15 00:18:50 +0200

    fix(release): update v2.0 package metadata

--- Formula/pleiades.rb
+++ Formula/pleiades.rb
@@ -1,27 +1,27 @@
 class Pleiades < Formula
-  desc "Provider-agnostic terminal AI assistant"
+  desc "Provider-agnostic autonomous terminal coding agent"
   homepage "https://github.com/CodWasTaken/Pleiades"
   head "https://github.com/CodWasTaken/Pleiades.git", branch: "master"
   license "MIT"
-  version "1.2.0"
+  version "2.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.2.0/pleiades-macos-arm64.tar.gz"
-      sha256 "d4551fbe8942ed1f891e264bc8a4f4e54f25796c015517b4bd523d15c03e3c0c"
+      url "https://github.com/CodWasTaken/Pleiades/releases/download/v2.0.0/pleiades-macos-arm64.tar.gz"
+      sha256 "f1e730cae00d99e7eaada6fd83840df23e13590402e7d8b868c01b779b055264"
     else
-      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.2.0/pleiades-macos-amd64.tar.gz"
-      sha256 "e3198c87282858b41496241f6c775f14fbd47c5986e8885449f447788f29c5ba"
+      url "https://github.com/CodWasTaken/Pleiades/releases/download/v2.0.0/pleiades-macos-amd64.tar.gz"
+      sha256 "5c841a7f9ffa8dea7e36320d703648aa0ad1e63ce4433f569eefca4e679f6bd3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.2.0/pleiades-linux-arm64.tar.gz"
-      sha256 "fca242fb48b617c5f7250e95bb74d14a82a4dc8fec9da63c3b12c069e86fb66c"
+      url "https://github.com/CodWasTaken/Pleiades/releases/download/v2.0.0/pleiades-linux-arm64.tar.gz"
+      sha256 "475d70f257751e851e9fae20f941ca9999915beba44b8123af6c3182b6dca59d"
     else
-      url "https://github.com/CodWasTaken/Pleiades/releases/download/v1.2.0/pleiades-linux-amd64.tar.gz"
-      sha256 "08cbe9a74284fa12c6ef4af202906e745135ee6abffe8c8e7aff4c90506f2b6b"
+      url "https://github.com/CodWasTaken/Pleiades/releases/download/v2.0.0/pleiades-linux-amd64.tar.gz"
+      sha256 "a016e3f78b2ee314a067e7fda1ee2c1b66642a69e06940f5d82c59fb60c6ea0e"
     end
   end
 
