366145748858c9f101cf891d763f0ed8b5112352
CodWasTaken <CodWasTaken@users.noreply.github.com>
2026-07-15 02:08:30 +0000

    deploy: bb2f79a703c42a42a736fe9e7a8cffe348b72041

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9d772bc4.js',
+                path_to_root + 'searchindex-ff972910.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
